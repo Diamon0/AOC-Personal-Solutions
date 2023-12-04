877adf6ae72a100087faf7d4200f0e9e4390a7f4
Diamon0 <79779017+Diamon0@users.noreply.github.com>
2023-12-04 17:02:28 -0500

    Added a bit of spacing for readability

--- Advent 2023/Source.cpp
+++ Advent 2023/Source.cpp
@@ -8,9 +8,11 @@
 int main() {
 	std::ifstream file = std::ifstream("CD.txt");
 	std::vector<uint32_t> calibrationValues = Calibration::findCalibrationValues(file);
+
 	for (uint32_t value : calibrationValues) {
 		std::cout << "Calibration Value: " << value << std::endl;
 	}
+
 	int sum = std::accumulate(calibrationValues.begin(), calibrationValues.end(), 0);
 	std::cout << "Sum of Calibration Values: " << sum << std::endl;
 }
