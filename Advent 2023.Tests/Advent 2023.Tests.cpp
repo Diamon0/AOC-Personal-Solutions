2e377ab59a7e48dc6dc4f118e2545b0b1b9234d6
Diamon0 <79779017+Diamon0@users.noreply.github.com>
2023-12-05 17:49:50 -0500

    Updated 2023 tests to account for a day 1 edge case

--- Advent 2023.Tests/Advent 2023.Tests.cpp
+++ Advent 2023.Tests/Advent 2023.Tests.cpp
@@ -16,7 +16,7 @@ namespace Advent2023Tests
 		TEST_METHOD(CalibrationValuesTest)
 		{
 			std::ifstream file = std::ifstream("CDE.txt");
-			std::array<uint32_t, 4> expectedValues = { 12, 38, 15, 77 };
+			std::array<uint32_t, 8> expectedValues = { 29, 83, 13, 24, 42, 14, 76, 62 };
 			std::vector<uint32_t> actualValues = Calibration::findCalibrationValues(file);
 
 			for (size_t i = 0; i < expectedValues.size(); i++) {
