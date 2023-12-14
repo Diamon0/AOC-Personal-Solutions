e544c39e91d92a56692cfc3c085112a592cd99ce
Diamon0 <79779017+Diamon0@users.noreply.github.com>
2023-12-14 18:06:36 -0500

    Day 2 Part 2 Main Cleanup

--- Advent 2023/Source.cpp
+++ Advent 2023/Source.cpp
@@ -7,24 +7,17 @@
 #include "CubeGame.h"
 
 int main() {
-	std::ifstream file = std::ifstream("GLE.txt");
+	std::ifstream file = std::ifstream("GL.txt");
 	CubeGame::CubeGames cubeGame = CubeGame::CubeGames::initializeGames(file);
 	file.close();
-
-	uint8_t maxReds = 12;
-	uint8_t maxGreens = 13;
-	uint8_t maxBlues = 14;
 
 	std::vector<CubeGame::Game> foundGames = cubeGame.games;
 
 	int sum = 0;
 
-	std::cout << "Found " << foundGames.size() << " Games with a maximum of " << (int)maxReds << " Reds, " << (int)maxGreens << " Greens, and " << (int)maxBlues << " Blues" << std::endl;
-	std::cout << "The IDs are:";
 	for (CubeGame::Game& game : foundGames) {
-		std::cout << " " << game.id;
 		sum += game.maxRedCount * game.maxGreenCount * game.maxBlueCount;
 	}
 	std::cout << std::endl;
-	std::cout << "The sum of all Game IDs is " << sum << std::endl;
+	std::cout << "The sum of every game's power is " << sum << std::endl;
 }
